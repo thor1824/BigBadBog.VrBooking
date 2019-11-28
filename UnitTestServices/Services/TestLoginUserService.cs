6f857a96afc8a15d58a612e2d670037dcea93f9a
gtafiper <43669870+gtafiper@users.noreply.github.com>
2019-11-28 13:33:39 +0100

    som service

---
+++ UnitTestServices/Services/TestLoginUserService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using VrBooking.Core;
+using VrBooking.Core.ApplicationServices;
+using VrBooking.Core.Entity;
+
+namespace TestServices.Services
+{
+    [TestClass]
+    public class TestLoginUserService
+    {
+        Mock<IRepository<LoginUser>> mockRepo;
+        private ILoginUserService service;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            mockRepo = new Mock<IRepository<LoginUser>>();
+            service = new LoginUserService(mockRepo.Object);
+        }
+
+        [TestMethod]
+        public void TestLoginUserCreate()
+        {
+            LoginUser userBeforeCreate = new LoginUser()
+            {
+                Activated = true,
+                Admin = false,
+                UserName = "perPerson@easv365.dk",
+                PasswordHash = new byte[] {1, 2, 3},
+                PasswordSalt = new byte[] {1, 2, 3}
+            };
+
+            LoginUser userAfterCreate = new LoginUser()
+            {
+                PasswordHash = userBeforeCreate.PasswordHash,
+                PasswordSalt = userBeforeCreate.PasswordSalt,
+                UserName = userBeforeCreate.UserName,
+                Activated = userBeforeCreate.Activated,
+                Admin = userBeforeCreate.Admin,
+                Id = 1
+            };
+
+            mockRepo.Setup(repo => repo.Create(userBeforeCreate)).Returns(userAfterCreate);
+            Assert.IsTrue(service.Create(userBeforeCreate) != null);
+            mockRepo.Verify(x => x.Create(It.IsAny<LoginUser>()), Times.Once);
+        }
+
+
+
+    }
+}
--- VrBooking.Core/ApplicationServices/Impl/LoginUserService.cs
+++ VrBooking.Core/ApplicationServices/Impl/LoginUserService.cs
@@ -1,21 +1,92 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using System.Text.RegularExpressions;
 using VrBooking.Core.Entity;
 
 namespace VrBooking.Core.ApplicationServices
 {
     public class LoginUserService : ILoginUserService
     {
+        private IRepository<LoginUser> _repo;
+
+        public LoginUserService(IRepository<LoginUser> repo)
+        {
+            this._repo = repo;
+        }
         public LoginUser Create(LoginUser loginUser)
         {
-            return null;
+            LoginUser createdLoginUser;
+            try
+            {
+                if (isUserNameNull(loginUser))
+                {
+                    throw new InvalidDataException("user must contain a name");
+                }
+
+                if (isUserNameValid(loginUser))
+                {
+                    throw new InvalidDataException("username contains special caterers");
+                }
+
+                
+                createdLoginUser = _repo.Create(loginUser);
+
+                if (isIdValid(createdLoginUser))
+                {
+                    throw new InvalidDataException("Id not valid");
+                }
+
+                if (dosContainPassword(createdLoginUser))
+                {
+                    throw new InvalidDataException("LoginUser dos not contain password");
+                }
+
+                if (dosContainSalt(createdLoginUser))
+                {
+                    throw new InvalidDataException("LoginUser dos not contain passwordSalt");
+                }
+
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+
+            return createdLoginUser;
         }
 
         public LoginUser Read(long id)
         {
-            return null;
+            LoginUser user;
+            try
+            {
+                user = _repo.Read(id);
+                {
+                    if (user == null)
+                    {
+                        throw new InvalidDataException("User does not exist");
+                    }
+                    if (user.Id != id)
+                    {
+                        throw new InvalidOperationException("Error: LoginUserService Read(id) retrieves wrong loginUser");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+            return user;
         }
+
         public List<LoginUser> ReadAll()
         {
             return null;
@@ -28,5 +99,98 @@ namespace VrBooking.Core.ApplicationServices
         {
             return null;
         }
+
+
+        #region validation 
+
+
+        public bool LoginUserExist(long id)
+        {
+            if (_repo.Read(id) == null)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public bool isIdValid(LoginUser loginUser)
+        {
+            if (loginUser.Id <= 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+
+            
+
+        }
+
+        public bool isUserNameNull(LoginUser user)
+        {
+            if (string.IsNullOrEmpty(user.UserName))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+
+        }
+
+        public bool isUserNameValid(LoginUser user)
+        {
+
+            var regexItem = new Regex("^([\\w\\.\\-_]+)@easv365.dk*$");
+
+
+            if (regexItem.IsMatch(user.UserName))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+
+    
+
+        public bool dosContainPassword(LoginUser user)
+        {
+            if (user.PasswordHash == null && user.PasswordHash.Length < 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public bool dosContainSalt(LoginUser user)
+        {
+            if (user.PasswordSalt == null && user.PasswordSalt.Length < 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+
+
+        #endregion
     }
 }
+//whitspaces tegn array toLower mellem 3 og 20 tegn 
+   // salt og password
